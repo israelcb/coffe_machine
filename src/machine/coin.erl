b247d04db9779d420c11f8ce807a0b1e527e898d
rozcovo <rozcovo@cpan.org>
2020-01-16 00:13:05 -0300

    Fixing error ocurred when you try to buy an 'americano' with two 0.50$ coins

--- src/machine/coin.erl
+++ src/machine/coin.erl
@@ -6,6 +6,9 @@
 -import(lists, [member/2]).
 -import(lists, [sort/1]).
 -import(lists, [reverse/1]).
+-import(lists, [search/2]).
+-import(lists, [merge/2]).
+-import(lists, [map/2]).
 
 -export([exists/1]).
 -export([total_value/1]).
@@ -31,9 +34,27 @@ subtract([], 0.00, ResultCoins) ->
 subtract([FirstCoin | Coins], 0.00, ResultCoins) ->
 	subtract(Coins, 0.00, [FirstCoin | ResultCoins]);
 subtract([], CoffePrice, ResultCoins) ->
-	subtract(ResultCoins, CoffePrice);
+	subtract(exchange_first_coin(ResultCoins), CoffePrice, []);
 subtract([Coin | Coins], CoffePrice, ResultCoins)
 when Coin =< CoffePrice ->
 	subtract(Coins, CoffePrice - Coin, ResultCoins);
 subtract([Coin | Coins], CoffePrice, ResultCoins) ->
 	subtract(Coins, CoffePrice, [Coin | ResultCoins]).
+
+exchange_first_coin([]) ->
+	[];
+exchange_first_coin([Coin | Coins]) ->
+	merge(exchange_coin(Coin), Coins).
+
+exchange_coin(Coin) ->
+	CoinRoundedValue = round(Coin * 100),
+	{value, MaxCoinValue} = search(
+		fun(C) -> C < CoinRoundedValue andalso CoinRoundedValue rem C == 0 end,
+		reverse(sort(map(fun(C) -> round(C * 100) end, ?COIN_VALUES)))),
+	io:format("Entrou -> ~w~n", [Coin / (MaxCoinValue / 100)]),
+	exchange_coin(MaxCoinValue / 100, round(Coin / (MaxCoinValue / 100)), []).
+
+exchange_coin(_, 0, Coins) ->
+	Coins;
+exchange_coin(CoinValue, RemainingCoins, Coins) ->
+	exchange_coin(CoinValue, RemainingCoins - 1, [CoinValue | Coins]).
